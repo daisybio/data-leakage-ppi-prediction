e3a99f79f3a1e96c5bea7cd7e6e340317265aafa
Judith Bernett <bernett@in.tum.de>
2022-08-22 11:36:33 +0200

    timed SPRINT

--- algorithms/SPRINT/run_SPRINT_rewired.sh
+++ algorithms/SPRINT/run_SPRINT_rewired.sh
@@ -4,10 +4,10 @@
 for DATASET in du guo
 do
   echo dataset ${DATASET}
-  bin/predict_interactions -p ../../Datasets_PPIs/SwissProt/yeast_swissprot_oneliner.fasta -h HSP/pre_computed_yeast_HSP -tr data/rewired/${DATASET}_train_pos.txt -pos data/rewired/${DATASET}_test_pos.txt -neg data/rewired/${DATASET}_test_neg.txt -o results/rewired/${DATASET}_results.txt
+  { time bin/predict_interactions -p ../../Datasets_PPIs/SwissProt/yeast_swissprot_oneliner.fasta -h HSP/pre_computed_yeast_HSP -tr data/rewired/${DATASET}_train_pos.txt -pos data/rewired/${DATASET}_test_pos.txt -neg data/rewired/${DATASET}_test_neg.txt -o results/rewired/${DATASET}_results.txt ; } 2> results/rewired/${DATASET}_time.txt
 done
 for DATASET in huang pan richoux_regular richoux_strict
 do
   echo dataset ${DATASET}
-  bin/predict_interactions -p data/Uniprot_human_protein_sequences.fasta -h HSP/pre_computed_HSP -tr data/rewired/${DATASET}_train_pos.txt -pos data/rewired/${DATASET}_test_pos.txt -neg data/rewired/${DATASET}_test_neg.txt -o results/rewired/${DATASET}_results.txt
+  { time bin/predict_interactions -p data/Uniprot_human_protein_sequences.fasta -h HSP/pre_computed_HSP -tr data/rewired/${DATASET}_train_pos.txt -pos data/rewired/${DATASET}_test_pos.txt -neg data/rewired/${DATASET}_test_neg.txt -o results/rewired/${DATASET}_results.txt ; } 2> results/rewired/${DATASET}_time.txt
 done
