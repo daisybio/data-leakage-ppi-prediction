0dfbdc5e07fbfb4a0cf0605b3316e71b61f45751
Judith Bernett <bernett@in.tum.de>
2023-04-18 12:31:29 +0200

    dscript slurm script

---
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_original.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=30
+#SBATCH --job-name=dscript_train
+#SBATCH --output=dscript_train.out
+#SBATCH --error=dscript_train.err
+#SBATCH --mem=50G
+
+
+for DATASET in guo du
+do
+  echo dataset ${DATASET}
+  { awk -F '\t' -v OFS='\t' '{$(NF+1) = 1; print }' ../SPRINT/data/original/${DATASET}_train_pos.txt && awk -F '\t' -v OFS='\t' '{$(NF+1) =  0; print }'  ../SPRINT/data/original/${DATASET}_train_neg.txt; } > tmp.txt
+  { time dscript train --train tmp.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models -o ./results_dscript/original/${DATASET}_train.txt; } 2> results_dscript/original/${DATASET}_time.txt
+  { time dscript train --topsy-turvy --train tmp.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models -o ./results_topsyturvy/original/${DATASET}_train.txt; } 2> results_topsyturvy/original/${DATASET}_time.txt
+done
+for DATASET in huang pan richoux_regular richoux_strict
+do
+  echo dataset ${DATASET}
+  { awk -F '\t' -v OFS='\t' '{$(NF+1) = 1; print }' ../SPRINT/data/original/${DATASET}_train_pos.txt && awk -F '\t' -v OFS='\t' '{$(NF+1) =  0; print }'  ../SPRINT/data/original/${DATASET}_train_neg.txt; } > tmp.txt
+  { time dscript train --train [training data] --val [validation data] --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models -o ./results_dscript/original/${DATASET}_train.txt; } 2> results_dscript/original/${DATASET}_time.txt
+  { time dscript train --topsy-turvy --train [training data] --val [validation data] --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models -o ./results_topsyturvy/original/${DATASET}_train.txt; } 2> results_topsyturvy/original/${DATASET}_time.txt
+done
