a61236b0d9be9225e0c7007a4c9c087518b7c226
Judith Bernett <bernett@in.tum.de>
2023-04-21 18:05:31 +0200

    ready to run

--- algorithms/D-SCRIPT-main/run_DSCRIPT_original_human.sh
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_original_human.sh
@@ -3,6 +3,9 @@
 for DATASET in huang pan richoux_regular richoux_strict
 do
   echo dataset ${DATASET}
-  { time dscript train --train data/original/${DATASET}_train.txt --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models -o ./results_dscript/original/${DATASET}_train.txt -d 0; } 2> results_dscript/original/${DATASET}_time.txt
-  { time dscript train --topsy-turvy --train data/original/${DATASET}_train.txt --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models -o ./results_topsyturvy/original/${DATASET}_train.txt -d 0; } 2> results_topsyturvy/original/${DATASET}_time.txt
+  { time dscript train --train data/original/${DATASET}_train.txt --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${DATASET}_dscript_original -o ./results_dscript/original/${DATASET}_train.txt -d 0; } 2> results_dscript/original/${DATASET}_time.txt
+  { time dscript evaluate --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --model ./models/${DATASET}_dscript_original_final.sav  -o ./results_dscript/original/${DATASET}.txt -d 0; } 2> results_dscript/original/${DATASET}_time_test.txt
+
+  { time dscript train --topsy-turvy --train data/original/${DATASET}_train.txt --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${DATASET}_tt_original -o ./results_topsyturvy/original/${DATASET}_train.txt -d 0; } 2> results_topsyturvy/original/${DATASET}_time.txt
+  { time dscript evaluate --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --model ./models/${DATASET}_tt_original_final.sav  -o ./results_topsyturvy/original/${DATASET}.txt -d 0; } 2> results_topsyturvy/original/${DATASET}_time_test.txt
 done
