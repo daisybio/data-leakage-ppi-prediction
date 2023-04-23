e55044e40780978e89ee2a9c2bed8a2decd0ee9e
Judith Bernett <bernett@in.tum.de>
2023-04-23 13:10:21 +0200

    typo in script

--- algorithms/D-SCRIPT-main/run_DSCRIPT_rewired_yeast.sh
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_rewired_yeast.sh
@@ -3,7 +3,7 @@
 for DATASET in guo du
 do
   echo dataset ${DATASET}
-  { time dscript train --train data/rewired/${DATASET}_train.txt --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_tt_rewired -o ./results_dscript/rewired/${DATASET}_train.txt -d 0; } 2> results_dscript/rewired/${DATASET}_time.txt
+  { time dscript train --train data/rewired/${DATASET}_train.txt --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_dscript_rewired -o ./results_dscript/rewired/${DATASET}_train.txt -d 0; } 2> results_dscript/rewired/${DATASET}_time.txt
   { time dscript evaluate --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_dscript_rewired_final.sav  -o ./results_dscript/rewired/${DATASET}.txt -d 0; } 2> results_dscript/rewired/${DATASET}_time_test.txt
 
   { time dscript train --topsy-turvy --train data/rewired/${DATASET}_train.txt --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_tt_rewired -o ./results_topsyturvy/rewired/${DATASET}_train.txt -d 0; } 2> results_topsyturvy/rewired/${DATASET}_time.txt
