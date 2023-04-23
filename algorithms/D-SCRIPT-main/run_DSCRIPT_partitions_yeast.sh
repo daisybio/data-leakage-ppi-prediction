fc5c4d21cd65d260db0146705fad6b7c49827c4a
Judith Bernett <bernett@in.tum.de>
2023-04-23 23:01:14 +0200

    typo in script

--- algorithms/D-SCRIPT-main/run_DSCRIPT_partitions_yeast.sh
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_partitions_yeast.sh
@@ -19,7 +19,7 @@ do
 
   echo train 0 test 1
   { time dscript train --train data/partitions/${DATASET}_partition_0.txt --test data/partitions/${DATASET}_partition_1.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_0_1_dscript_partitions -o ./results_dscript/partitions/${DATASET}_partition_0_1.txt -d 0; } 2> results_dscript/partitions/${DATASET}_partition_0_1_time.txt
-  { time dscript evaluate --test data/partitions/${DATASET}_partition_1.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_0_1_dscript_partitions_final.sav  -o ./results_dscript/partitions/${DATASET}_both_1.txt -d 0; } 2> results_dscript/partitions/${DATASET}_both_1_time.txt
+  { time dscript evaluate --test data/partitions/${DATASET}_partition_1.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_0_1_dscript_partitions_final.sav  -o ./results_dscript/partitions/${DATASET}_0_1.txt -d 0; } 2> results_dscript/partitions/${DATASET}_0_1_time.txt
 
   { time dscript train --topsy-turvy --train data/partitions/${DATASET}_partition_0.txt --test data/partitions/${DATASET}_partition_1.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_0_1_tt_partitions -o ./results_topsyturvy/partitions/${DATASET}_partition_0_1.txt -d 0; } 2> results_topsyturvy/partitions/${DATASET}_partition_0_1_time.txt
   { time dscript evaluate --test data/partitions/${DATASET}_partition_1.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_0_1_tt_partitions_final.sav  -o ./results_topsyturvy/partitions/${DATASET}_0_1.txt -d 0; } 2> results_topsyturvy/partitions/${DATASET}_0_1_time.txt
