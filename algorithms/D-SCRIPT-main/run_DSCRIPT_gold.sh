c6a50158c7e51193f5aaf501e8a1b49255a114f2
Judith Bernett <bernett@in.tum.de>
2023-04-24 12:22:44 +0200

    goldstandard script

---
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_gold.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo dscript
+{ time dscript train --train data/original/${DATASET}_train.txt --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${DATASET}_dscript_original -o ./results_dscript/original/${DATASET}_train.txt -d 1; } 2> results_dscript/original/${DATASET}_time.txt
+{ time dscript evaluate --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --model ./models/${DATASET}_dscript_original_final.sav  -o ./results_dscript/original/${DATASET}.txt -d 1; } 2> results_dscript/original/${DATASET}_time_test.txt
+
+echo topsyturvy
+{ time dscript train --topsy-turvy --train data/original/${DATASET}_train.txt --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${DATASET}_tt_original -o ./results_topsyturvy/original/${DATASET}_train.txt -d 1; } 2> results_topsyturvy/original/${DATASET}_time.txt
+{ time dscript evaluate --test data/original/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --model ./models/${DATASET}_tt_original_final.sav  -o ./results_topsyturvy/original/${DATASET}.txt -d 1; } 2> results_topsyturvy/original/${DATASET}_time_test.txt
