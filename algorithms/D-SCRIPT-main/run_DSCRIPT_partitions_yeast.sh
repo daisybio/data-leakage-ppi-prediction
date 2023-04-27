e311d1020d1fcee6cec72d6c1287de46d65571e2
Judith Bernett <bernett@in.tum.de>
2023-04-27 10:28:42 +0200

    getting scripts ready for re-running partitions for dscript - other cuda device

--- algorithms/D-SCRIPT-main/run_DSCRIPT_partitions_yeast.sh
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_partitions_yeast.sh
@@ -13,12 +13,12 @@ do
       fi
       echo dataset ${DATASET}, training on ${TRAIN}, testing on ${TEST}
       echo DSCRIPT
-      { time dscript train --train data/partitions/${DATASET}_partition_${TRAIN}.txt --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_${TRAIN}_${TEST}_dscript_partitions -o ./results_dscript/partitions/train_${DATASET}_${TRAIN}_${TEST}.txt -d 2; } 2> results_dscript/partitions/train_${DATASET}_${TRAIN}_${TEST}_time.txt
-      { time dscript evaluate --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_${TRAIN}_${TEST}_dscript_partitions_final.sav  -o ./results_dscript/partitions/${DATASET}_${TRAIN}_${TEST} -d 2; } 2> results_dscript/partitions/${DATASET}_${TRAIN}_${TEST}_time.txt
+      { time dscript train --train data/partitions/${DATASET}_partition_${TRAIN}.txt --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_${TRAIN}_${TEST}_dscript_partitions -o ./results_dscript/partitions/train_${DATASET}_${TRAIN}_${TEST}.txt -d 1; } 2> results_dscript/partitions/train_${DATASET}_${TRAIN}_${TEST}_time.txt
+      { time dscript evaluate --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_${TRAIN}_${TEST}_dscript_partitions_final.sav  -o ./results_dscript/partitions/${DATASET}_${TRAIN}_${TEST} -d 1; } 2> results_dscript/partitions/${DATASET}_${TRAIN}_${TEST}_time.txt
 
       echo TOPSYTURVY
-      { time dscript train --topsy-turvy --train data/partitions/${DATASET}_partition_${TRAIN}.txt --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_${TRAIN}_${TEST}_tt_partitions -o ./results_topsyturvy/partitions/train_${DATASET}_${TRAIN}_${TEST}.txt -d 2; } 2> results_topsyturvy/partitions/train_${DATASET}_${TRAIN}_${TEST}_time.txt
-      { time dscript evaluate --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_${TRAIN}_${TEST}_tt_partitions_final.sav  -o ./results_topsyturvy/partitions/${DATASET}_${TRAIN}_${TEST} -d 2; } 2> results_topsyturvy/partitions/${DATASET}_${TRAIN}_${TEST}_time.txt
+      { time dscript train --topsy-turvy --train data/partitions/${DATASET}_partition_${TRAIN}.txt --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --save-prefix ./models/${DATASET}_${TRAIN}_${TEST}_tt_partitions -o ./results_topsyturvy/partitions/train_${DATASET}_${TRAIN}_${TEST}.txt -d 1; } 2> results_topsyturvy/partitions/train_${DATASET}_${TRAIN}_${TEST}_time.txt
+      { time dscript evaluate --test data/partitions/${DATASET}_partition_${TEST}.txt --embedding /nfs/scratch/jbernett/yeast_embedding.h5 --model ./models/${DATASET}_${TRAIN}_${TEST}_tt_partitions_final.sav  -o ./results_topsyturvy/partitions/${DATASET}_${TRAIN}_${TEST} -d 1; } 2> results_topsyturvy/partitions/${DATASET}_${TRAIN}_${TEST}_time.txt
     done
   done
 done
