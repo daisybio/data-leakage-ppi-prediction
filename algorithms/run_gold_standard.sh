d3091208d2b023a986f29c41b0229339b72912f7
Judith Bernett <bernett@in.tum.de>
2023-05-08 11:02:17 +0200

    preparing to run unbalanced gold standard dataset

--- algorithms/run_gold_standard.sh
+++ algorithms/run_gold_standard.sh
@@ -11,24 +11,24 @@
 # Custom
 cd Custom
 echo Custom
-python run.py gold_standard
+python run.py gold_standard_unbalanced
 cd ..
 #DeepFE-PPI
 cd DeepFE-PPI
 echo DeepFE
-python train_all_datasets.py gold_standard
+python train_all_datasets.py gold_standard_unbalanced
 cd ..
 #Richoux-FC
 cd DeepPPI/keras
 echo Richoux_FC
-python train_all_datasets.py -name FC_gold_standard -train_pos ../../../Datasets_PPIs/Hippiev2.3/Intra1_pos_rr.txt -train_neg ../../../Datasets_PPIs/Hippiev2.3/Intra1_neg_rr.txt -val_pos ../../../Datasets_PPIs/Hippiev2.3/Intra0_pos_rr.txt -val_neg ../../../Datasets_PPIs/Hippiev2.3/Intra0_neg_rr.txt -test_pos ../../../Datasets_PPIs/Hippiev2.3/Intra2_pos_rr.txt -test_neg ../../../Datasets_PPIs/Hippiev2.3/Intra2_neg_rr.txt -model fc2_20_2dense -epochs 25 -batch 2048
+python train_all_datasets.py -name FC_gold_standard_unbalanced -train_pos ../../../Datasets_PPIs/unbalanced_gold/Intra1_pos.txt -train_neg ../../../Datasets_PPIs/unbalanced_gold/Intra1_neg.txt -val_pos ../../../Datasets_PPIs/unbalanced_gold/Intra0_pos.txt -val_neg ../../../Datasets_PPIs/unbalanced_gold/Intra0_neg.txt -test_pos ../../../Datasets_PPIs/unbalanced_gold/Intra2_pos.txt -test_neg ../../../Datasets_PPIs/unbalanced_gold/Intra2_neg.txt -model fc2_20_2dense -epochs 25 -batch 2048
 #Richoux-LSTM
 echo Richoux_LSTM
-python train_all_datasets.py -name LSTM_gold_standard -train_pos ../../../Datasets_PPIs/Hippiev2.3/Intra1_pos_rr.txt -train_neg ../../../Datasets_PPIs/Hippiev2.3/Intra1_neg_rr.txt -val_pos ../../../Datasets_PPIs/Hippiev2.3/Intra0_pos_rr.txt -val_neg ../../../Datasets_PPIs/Hippiev2.3/Intra0_neg_rr.txt -test_pos ../../../Datasets_PPIs/Hippiev2.3/Intra2_pos_rr.txt -test_neg ../../../Datasets_PPIs/Hippiev2.3/Intra2_neg_rr.txt -model lstm32_3conv3_2dense_shared -epochs 100 -batch 2048
+python train_all_datasets.py -name LSTM_gold_standard_unbalanced -train_pos ../../../Datasets_PPIs/unbalanced_gold/Intra1_pos.txt -train_neg ../../../Datasets_PPIs/unbalanced_gold/Intra1_neg.txt -val_pos ../../../Datasets_PPIs/unbalanced_gold/Intra0_pos.txt -val_neg ../../../Datasets_PPIs/unbalanced_gold/Intra0_neg.txt -test_pos ../../../Datasets_PPIs/unbalanced_gold/Intra2_pos.txt -test_neg ../../../Datasets_PPIs/unbalanced_gold/Intra2_neg.txt -model lstm32_3conv3_2dense_shared -epochs 100 -batch 2048
 cd ../..
 #SPRINT
-cat ../Datasets_PPIs/Hippiev2.3/Intra0_pos_rr.txt ../Datasets_PPIs/Hippiev2.3/Intra1_pos_rr.txt > ../Datasets_PPIs/Hippiev2.3/Intra0_Intra1_pos_rr.txt
+cat ../Datasets_PPIs/unbalanced_gold/Intra0_pos.txt ../Datasets_PPIs/unbalanced_gold/Intra1_pos.txt > ../Datasets_PPIs/unbalanced_gold/Intra0_Intra1_unbalanced_pos.txt
 cd SPRINT
 echo SPRINT
-{ time bin/predict_interactions -p data/Uniprot_human_protein_sequences.fasta -h HSP/pre_computed_HSP -tr ../../Datasets_PPIs/Hippiev2.3/Intra0_Intra1_pos_rr.txt -pos ../../Datasets_PPIs/Hippiev2.3/Intra2_pos_rr.txt -neg ../../Datasets_PPIs/Hippiev2.3/Intra2_neg_rr.txt -o results/original/gold_standard_results.txt ; } 2> results/original/gold_standard_time.txt
+{ time bin/predict_interactions -p data/Uniprot_human_protein_sequences.fasta -h HSP/pre_computed_HSP -tr ../Datasets_PPIs/unbalanced_gold/Intra0_Intra1_unbalanced_pos.txt -pos ../Datasets_PPIs/unbalanced_gold/Intra2_pos.txt -neg ../Datasets_PPIs/unbalanced_gold/Intra2_neg.txt -o results/original/gold_standard_unbalanced_results.txt ; } 2> results/original/gold_standard_unbalanced_time.txt
 
