4a94e966c4c1c515142c31a1a5ad83fbe413b51b
JudithBernett <bernett@in.tum.de>
2023-08-21 11:35:24 +0200

    new batch of results

--- algorithms/D-SCRIPT-main/run_DSCRIPT_rewired_human.sh
+++ algorithms/D-SCRIPT-main/run_DSCRIPT_rewired_human.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=1
+#SBATCH --job-name=train_ds_rew_human
+#SBATCH --output=train_ds_rewired_human%j.out
+#SBATCH --error=train_ds_rewired_human%j.err
+#SBATCH --mem=300G
+#SBATCH --time=48:00:00
+#SBATCH --partition=compms-gpu-a40
+#SBATCH --array=0-3
 
-for DATASET in dscript #huang pan richoux_regular richoux_strict
-do
-  echo dataset ${DATASET}
-  { time dscript train --train data/rewired/${DATASET}_train.txt --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${DATASET}_dscript_rewired -o ./results_dscript/rewired/${DATASET}_train.txt -d 1; } 2> results_dscript/rewired/${DATASET}_time.txt
-  { time dscript evaluate --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --model ./models/${DATASET}_dscript_rewired_final.sav  -o ./results_dscript/rewired/${DATASET}.txt -d 1; } 2> results_dscript/rewired/${DATASET}_time_test.txt
+declare -a datasets=(huang pan richoux_regular richoux_strict)
+{ time dscript train --train data/rewired/${datasets[${SLURM_ARRAY_TASK_ID}]}_train.txt --test data/rewired/${datasets[${SLURM_ARRAY_TASK_ID}]}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${datasets[${SLURM_ARRAY_TASK_ID}]}_dscript_rewired -o ./results_dscript/rewired/${datasets[${SLURM_ARRAY_TASK_ID}]}_train.txt; } 2> results_dscript/rewired/${datasets[${SLURM_ARRAY_TASK_ID}]}_time.txt
 
-  { time dscript train --topsy-turvy --train data/rewired/${DATASET}_train.txt --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --save-prefix ./models/${DATASET}_tt_rewired -o ./results_topsyturvy/rewired/${DATASET}_train.txt -d 1; } 2> results_topsyturvy/rewired/${DATASET}_time.txt
-  { time dscript evaluate --test data/rewired/${DATASET}_test.txt --embedding /nfs/scratch/jbernett/human_embedding.h5 --model ./models/${DATASET}_tt_rewired_final.sav  -o ./results_topsyturvy/rewired/${DATASET}.txt -d 1; } 2> results_topsyturvy/rewired/${DATASET}_time_test.txt
-done
