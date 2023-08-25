2f472616c150331c4ae7df16d3ebb35446112061
Judith Bernett <bernett@in.tum.de>
2023-08-25 10:33:44 +0200

    preparing dscript and topsyturvy scripts for early stopping evaluation

--- algorithms/D-SCRIPT-main/evaluate_dscript_es.sh
+++ algorithms/D-SCRIPT-main/evaluate_dscript_es.sh
@@ -2,22 +2,22 @@
 #
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --gpus-per-task=1
 #SBATCH --job-name=eval_dscript
-#SBATCH --output=evaluate_dscript_original.out
-#SBATCH --error=evaluate_dscript_original.err
+#SBATCH --output=evaluate_dscript.out
+#SBATCH --error=evaluate_dscript.err
 #SBATCH --mem=300G
 #SBATCH --time=24:00:00
-#SBATCH --partition=jlab-gpu01
+#SBATCH --partition=shared-cpu
+#SBATCH --array=0-31
 
 echo evaluate d-script with manual early stopping. See best epochs in table
-echo ORIGINAL
+declare -a combis
 
-declare -A bestepochs=( ["huang"]="05" ["guo"]="10" ["du"]="09" ["pan"]="09" ["richoux_regular"]="01" ["richoux_strict"]="01" ["dscript"]="04" ["gold"]="02" )
+declare -A bestepochs=( ["huang"]="05" ["guo"]="10" ["du"]="09" ["pan"]="09" ["richoux_regular"]="01" ["richoux_strict"]="01" ["dscript"]="07" )
 
-for DATASET in huang guo du pan richoux_regular richoux_strict dscript gold
+index=0
+for DATASET in huang guo du pan richoux_regular richoux_strict dscript
 do
-	echo $DATASET
 	if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
                 EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
         else
@@ -26,89 +26,86 @@ do
         echo $EMBEDDING
 	
 	EPOCH=${bestepochs[$DATASET]}
-	if [ "$DATASET" == "gold" ]; then
-                MODEL="./models/dscript_${DATASET}_epoch${EPOCH}.sav"
-        else
-                MODEL="./models/${DATASET}_dscript_original_epoch${EPOCH}.sav"
-        fi
-	echo evaluating on model $MODEL
-
-	dscript evaluate --test data/original/${DATASET}_test.txt --embedding $EMBEDDING --model $MODEL -o ~/tmp_eval_out.txt
+	MODEL="./models/${DATASET}_dscript_original_epoch${EPOCH}.sav"
+	TESTSET="data/original/${DATASET}_test.txt"
+	OUTFILE="./results_dscript/original/${DATASET}_es.txt"
+  combis[$index]="$TESTSET $EMBEDDING $MODEL $OUTFILE"
+  index=$((index+1))
 done
 
-#echo REWIRED
-#declare -A bestepochs=( ["huang"]="10" ["guo"]="10" ["du"]="01" ["pan"]="02" ["richoux_regular"]="02" ["richoux_strict"]="01" ["dscript"]="06" )
+declare -A bestepochs=( ["huang"]="08" ["guo"]="09" ["du"]="09" ["pan"]="06" ["richoux_regular"]="01" ["richoux_strict"]="01" ["dscript"]="02" )
+for DATASET in huang guo du pan richoux_regular richoux_strict dscript
+do
+        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
+                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
+        else
+                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
+        fi
 
-#for DATASET in huang guo du pan richoux_regular richoux_strict dscript
-#do
-#        echo $DATASET
-#        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
-#                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
-#        else
-#                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
-#        fi
-#        echo $EMBEDDING
-#
-#        EPOCH=${bestepochs[$DATASET]}
-#        MODEL="./models/${DATASET}_dscript_rewired_epoch${EPOCH}.sav"
-#        echo evaluating on model $MODEL
-#
-#        dscript evaluate --test data/rewired/${DATASET}_test.txt --embedding $EMBEDDING --model $MODEL -o ./results_dscript/rewired/${DATASET}_es.txt -d 1
-#done
+        EPOCH=${bestepochs[$DATASET]}
+        MODEL="./models/${DATASET}_dscript_rewired_epoch${EPOCH}.sav"
+        TESTSET="data/rewired/${DATASET}_test.txt"
+        OUTFILE="./results_dscript/rewired/${DATASET}_es.txt"
+        combis[$index]="$TESTSET $EMBEDDING $MODEL $OUTFILE"
+        index=$((index+1))
+done
 
-#echo PARTITION both_0
-#declare -A bestepochs=( ["huang"]="01" ["guo"]="09" ["du"]="07" ["pan"]="05" ["richoux"]="04" ["dscript"]="10" )
-#for DATASET in huang guo du pan richoux dscript 
-#do
-#        echo $DATASET
-#        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
-#                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
-#        else
-#                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
-#        fi
-#        echo $EMBEDDING
+declare -A bestepochs=( ["huang"]="08" ["guo"]="09" ["du"]="08" ["pan"]="06" ["richoux"]="03" ["dscript"]="02" )
+for DATASET in huang guo du pan richoux dscript
+do
+        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
+                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
+        else
+                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
+        fi
 
-#        EPOCH=${bestepochs[$DATASET]}
-#        MODEL="./models/${DATASET}_both_0_dscript_partitions_epoch${EPOCH}.sav"
-#        echo evaluating on model $MODEL
-#
-#        dscript evaluate --test data/partitions/${DATASET}_partition_0.txt --embedding $EMBEDDING --model $MODEL -o ./results_dscript/partitions/${DATASET}_both_0_es.txt -d 1
-#done
+        EPOCH=${bestepochs[$DATASET]}
+        MODEL="./models/${DATASET}_both_0_dscript_partitions_epoch${EPOCH}.sav"
+        TESTSET="data/partitions/${DATASET}_partition_0.txt"
+        OUTFILE="./results_dscript/partitions/${DATASET}_both_0_es.txt"
+        combis[$index]="$TESTSET $EMBEDDING $MODEL $OUTFILE"
+        index=$((index+1))
+done
 
-#echo PARTITION both_1
-#declare -A bestepochs=( ["huang"]="01" ["guo"]="03" ["du"]="07" ["pan"]="09" ["richoux"]="04" ["dscript"]="05" )
-#for DATASET in huang guo du pan richoux dscript                       
-#do
-#        echo $DATASET
-#        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
-#                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
-#        else
-#                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
-#        fi
-#        echo $EMBEDDING
-#
-#        EPOCH=${bestepochs[$DATASET]}
-#        MODEL="./models/${DATASET}_both_1_dscript_partitions_epoch${EPOCH}.sav"
-#        echo evaluating on model $MODEL
-#
-#        dscript evaluate --test data/partitions/${DATASET}_partition_1.txt --embedding $EMBEDDING --model $MODEL -o ./results_dscript/partitions/${DATASET}_both_1_es.txt -d 1
-#done
-#
-#echo PARTITION 0_1
-#declare -A bestepochs=( ["huang"]="08" ["guo"]="01" ["du"]="07" ["pan"]="09" ["richoux"]="01" ["dscript"]="01" )
-#for DATASET in huang guo du pan richoux dscript
-#do
-#        echo $DATASET
-#        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
-#                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
-#        else
-#                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
-#        fi
-#        echo $EMBEDDING
-#
-#        EPOCH=${bestepochs[$DATASET]}
-#        MODEL="./models/${DATASET}_0_1_dscript_partitions_epoch${EPOCH}.sav"
-#        echo evaluating on model $MODEL
-#
-#        dscript evaluate --test data/partitions/${DATASET}_partition_1.txt --embedding $EMBEDDING --model $MODEL -o ./results_dscript/partitions/${DATASET}_0_1_es.txt -d 1
-#done
+declare -A bestepochs=( ["huang"]="10" ["guo"]="10" ["du"]="05" ["pan"]="08" ["richoux"]="02" ["dscript"]="01" )
+for DATASET in huang guo du pan richoux dscript
+do
+        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
+                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
+        else
+                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
+        fi
+
+        EPOCH=${bestepochs[$DATASET]}
+        MODEL="./models/${DATASET}_both_1_dscript_partitions_epoch${EPOCH}.sav"
+        TESTSET="data/partitions/${DATASET}_partition_1.txt"
+        OUTFILE="./results_dscript/partitions/${DATASET}_both_1_es.txt"
+        combis[$index]="$TESTSET $EMBEDDING $MODEL $OUTFILE"
+        index=$((index+1))
+done
+
+declare -A bestepochs=( ["huang"]="06" ["guo"]="01" ["du"]="03" ["pan"]="10" ["richoux"]="04" ["dscript"]="01" )
+for DATASET in huang guo du pan richoux dscript
+do
+        if [[ "$DATASET" == "guo" ||  "$DATASET" == "du" ]] ; then
+                EMBEDDING='/nfs/scratch/jbernett/yeast_embedding.h5'
+        else
+                EMBEDDING='/nfs/scratch/jbernett/human_embedding.h5'
+        fi
+
+        EPOCH=${bestepochs[$DATASET]}
+        MODEL="./models/${DATASET}_0_1_dscript_partitions_epoch${EPOCH}.sav"
+        TESTSET="data/partitions/${DATASET}_partition_1.txt"
+        OUTFILE="./results_dscript/partitions/${DATASET}_0_1_es.txt"
+        combis[$index]="$TESTSET $EMBEDDING $MODEL $OUTFILE"
+        index=$((index+1))
+done
+
+parameters=(${combis[${SLURM_ARRAY_TASK_ID}]})
+testset=${parameters[0]}
+embedding=${parameters[1]}
+model=${parameters[2]}
+outfile=${parameters[3]}
+echo testset $testset embedding $embedding model $model outfile $outfile
+
+dscript evaluate --test $testset  --embedding $embedding --model $model -o $outfile
