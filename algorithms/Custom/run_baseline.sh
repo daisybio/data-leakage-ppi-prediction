76ca21e215ff90a50209476a28a3267a20f3bb4c
JudithBernett <bernett@in.tum.de>
2023-12-21 09:38:04 +0100

    results from multiple runs with different splits original rewired

--- algorithms/Custom/run_baseline.sh
+++ algorithms/Custom/run_baseline.sh
@@ -3,11 +3,13 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=7
 #SBATCH --job-name=baseML
-#SBATCH --output=baselineML.out
-#SBATCH --error=baselineML.err
+#SBATCH --output=baselineML_%A_%a.out
+#SBATCH --error=baselineML_%A_%a.err
 #SBATCH --mem=40G
+#SBATCH --array=0-3
 
-python run.py original
-python run.py rewired
-python run.py partition
-python run.py gold
+declare -a settings
+settings=(original rewired partition gold_standard)
+echo ${settings[${SLURM_ARRAY_TASK_ID}]}
+
+python run.py ${settings[${SLURM_ARRAY_TASK_ID}]}
